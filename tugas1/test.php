121cf357af9f8029036381f5311f3aabb0b29e5b
zulfatunnisa <80380143+zulfatunnisa@users.noreply.github.com>
2022-03-01 22:35:37 +0700

    Add files via upload

---
+++ tugas1/test.php
@@ -0,0 +1,23 @@
+<?php
+
+  #for($i=1; $i<=10; $i++) {
+  #  print("Urutan ke-$i\n");
+  #}
+  
+  $nim = "20090143";
+  $nama = "Zulfatun Nisa";
+  $kelas = "4D";
+  echo 'Hai Selamat Datang !<br/>';
+  echo "Perkenalkan <br/>
+  Nama Saya : $nama <br/>
+  NIM saya : $nim <br/>
+  Kelas : $kelas <br/>";
+
+  #$output = shell_exec("ls -lart");
+  #echo $output;
+
+  #shell_exec("touch index.php");
+  #shell_exec("echo '<h1>Hai, Apa kabar</h1>' > index.php");
+  print("Salam Kenal dari Saya $nama\n");
+  
+?>
